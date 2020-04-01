93490ffee2ad37cdd0e52eaa52466633a1af6ae8
Ubuntu <ubuntu@ip-172-31-44-91.ap-northeast-1.compute.internal>
2020-04-01 12:28:45 +0000

    Q4-8の回答を記載

--- task.rb
+++ task.rb
@@ -49,13 +49,19 @@ def q5
   array2 = [1, 5, 8, 10]
 
   # 以下に回答を記載
-
+  p array1.empty?
+  p array2.empty?
 end
 
 def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
+  numbers2 = []
+  numbers1.each do |n|
+    numbers2 << n*10
+  end
+  p numbers2
 
 end
 
@@ -63,6 +69,9 @@ def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
+  array.each_index do |i|
+    array[i] = array[i].to_i
+  end
 
   # 以下は変更しないで下さい
   p array
@@ -72,7 +81,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-
+  programming_languages.map(&:capitalize!)
+  upper_case_programming_languages = programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
   p upper_case_programming_languages
