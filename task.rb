ac81995efae6ae88e7c37aa81c4c4c4f62633235
great084 <great084@gmail.com>
2020-04-02 11:33:21 +0900

    Q14の回答を記載

--- task.rb
+++ task.rb
@@ -145,6 +145,9 @@ def q14
   data = { name: "satou", age: 33, address: "saitama", hobby: "soccer", email: "hoge@fuga.com" }
 
   # 以下に回答を記載
+  keys = []
+  data.each_key{ |key| keys << key}
+  p keys
 
 end
 
