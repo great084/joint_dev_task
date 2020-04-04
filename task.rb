abeaaa9dfabee814a1f3342fc4aa8c7bde6b1567
great084 <great084@gmail.com>
2020-04-05 01:53:48 +0900

    Q20の回答を実施

--- task.rb
+++ task.rb
@@ -249,12 +249,39 @@ end
 
 class UserQ20
   # 以下に回答を記載
+  attr_reader :name, :age
+
+  def initialize(name:, age:) #キーワード引数
+    @name = name
+    @age = age
+  end
 
 end
 
 class Zoo
   # 以下に回答を記載
+  def initialize(**arg)  #オプション引数
+    zoo_name = arg[:name]
+    @entry_fee_infant = arg[:entry_fee][:infant]
+    @entry_fee_children = arg[:entry_fee][:children]
+    @entry_fee_adult = arg[:entry_fee][:adult]
+    @entry_fee_senior = arg[:entry_fee][:senior]
+    # puts @entry_fee_children
+  end
 
+  def info_entry_fee(user)
+      case user.age
+      when 0..5
+        entry_fee = @entry_fee_infant
+      when 6..12
+        entry_fee = @entry_fee_children
+      when 13..64
+        entry_fee = @entry_fee_adult
+      when 65..120
+        entry_fee = @entry_fee_senior
+      end
+      puts "#{user.name}さんの入場料金は #{entry_fee} 円です。"
+  end
 end
 
 
